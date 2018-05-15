70e52b41a7fb8586e9028a813f5a5214f9ca8c45
Analia Mok <axm2580@g.rit.edu>
2018-05-14 23:13:06 -0400

    Added simple api call to news api

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,14 +1,25 @@
 import React, { Component } from 'react'
 import './styles/Home.css';
 
+let baseApiUrl = 'https://newsapi.org/v2';
+
 export default class Home extends Component {
-  constructor(props){
+  constructor(props) {
     super(props);
-    this.state = {
-      baseApiUrl: 'https://newsapi.org/v2',
-      apiKey: props.apiKey
-    };
+    // this.state = {
+    //   apiKey: props.apiKey
+    // };
   }
+
+  componentDidMount() {
+    let headlinesUrl = baseApiUrl + '/top-headlines?language=en&apiKey=' + this.props.apiKey;
+    fetch(headlinesUrl)
+      .then(res => res.json())
+      .then((result) => {
+        console.log(result);
+      })
+  }
+
   render() {
     return (
       <div>
