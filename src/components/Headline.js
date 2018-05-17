de2fdeaf385729f5a2a7b649232f92e6a9db1884
Analia Mok <axm2580@g.rit.edu>
2018-05-16 23:48:00 -0400

    Added excerpt to headlines box. Added some additional spacing

--- src/components/Headline.js
+++ src/components/Headline.js
@@ -7,9 +7,10 @@ const Headline = ({headline}) => (
   <a href={headline.url} target="_blank" rel="nofollow" className="headlines__headline">
     <span className="headlines__headline__source">{headline.source.name}</span>
     <div className="headlines__headline__image-container">
-      <img src={headline.urlToImage === null ? placeholderImg : headline.urlToImage} alt={headline.title}/>
+      <img src={headline.urlToImage === null || headline.urlToImage === "self" ? placeholderImg : headline.urlToImage} alt={headline.title}/>
     </div>
-    <h3 className="headlines__headline__title">{headline.title}</h3>
+    <h3 className="headlines__headline__title">{headline.title.replace(/<(.|\n)*?>/g, '')}</h3>
+    <p className="headlines__headline__excerpt">{headline.description.substr(0, 150)}{headline.description.length > 150 ? <span>...</span> : ''}</p>
   </a>
 );
 
